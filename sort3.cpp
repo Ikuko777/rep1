dee71c1984d1982592b23cee74bc10dd3ae21faf
IkukoHiraga <Ikuko@Ikuko-H.local>
2016-06-02 18:58:08 +0900

    修正版

--- sort3.cpp
+++ sort3.cpp
@@ -7,13 +7,10 @@ using namespace std;
 int hantei=0;
 FILE *fp;
 
-  
-
-
 //ソート済みのvectorとその時の文字数を渡されたら、アナグラムがあれば出力
-int anagram (vector<char> c,int j){
+int anagram (vector<char> &c,int j){
  
-  //printf("きたよ");
+  //printf("きたよ\n");
 
   char a;
   vector<char> d,e;
@@ -22,42 +19,33 @@ int anagram (vector<char> c,int j){
   c.reserve(1000);
   int l,k,i;
 
-  int test;
-  /*
-  for(i=0;i<10;i++){
-    printf("%c",c[i]);
-    }*/
-  
   l=0;
+
+  rewind(fp);
+  //fseek(fp,0,SEEK_SET);
+
+  if((a=getc(fp)) == EOF){printf("ファイルを開けません；；\n");}
+  
   while((a=getc(fp)) != EOF){
-    if(a!= '\n'){
+    if(a != '\n'){
       d[l]=a;
       l++;
     }else{
       l=l-1;
-      //printf("%d\n",l);
-      e.assign(&d[0],&d[l]);
-      sort(&d[0],&d[l]);
-      //printf("います");
-      /*for(i=0;i<10;i++){
-    printf("%c",d[i]);
-    }*/
-      if((j==l)&&(equal(&c[0],&c[j],&d[0]))){
-	for(k=0;k<l;k++){
-	  printf("%c",e[k]);
-	printf("入ってる？");
-	} 
-
-	test = 1;
-	hantei =1;
-	return test;
-	printf("終わるよ");
+      
+      e.assign(&d[0],&d[l+1]);
+      sort(&d[0],&d[l+1]);
    
+      if((j==l)&&(equal(&c[0],&c[l],&d[0]))){ 
+	for(k=0;k<l+1;k++){
+	  printf("%c",e[k]);
+	} 
+	hantei =1;
+	return 1;	
       }
       l=0;
     }
-  }
-
+  } 
   return 0;
 }
 
@@ -70,14 +58,14 @@ int main(){
   char a;
   int i,j;
   
-  //fp = fopen("/usr/share/dict/words","r");
-  fp = fopen("sort2.txt","r");
+  fp = fopen("/usr/share/dict/words","r");
+  //fp = fopen("sort2.txt","r");
   if(fp==NULL){
     printf("ファイル開けなかったよ；；\n");
     return 0;
   }
   
-
+  
   
   printf("16文字入力してね♪：");
   for(i=0;i<n+1;i++){        
@@ -95,48 +83,32 @@ int main(){
     printf("%c",c[i]);
   }
   printf("\n");
- 
-
-
-  //anagram(c);
-  printf("\n");
-
-
-
-  for(j=0;j<c.size();j++){
-    
   
+  
+  
+  for(j=0;j<c.size();j++){
+    printf("削除する文字数：%d\n",j);
+    
 
-  do {
-
-    for ( int i=0; i<c.size()-j; i++ ) {
-      // printf("%c ",c[i]);
-      d[i]=c[i];
-	}
-
-    sort(&d[0],&d[c.size()]);
-
-  for(i=0;i<c.size()+j;i++){
-    printf("%c",c[i]);
+    do {
+      
+      for ( int i=0; i<c.size()-j; i++ ) {
+	
+	d[i]=c[i];
+      }
+      
+      sort(&d[0],&d[c.size()]);
+      
+      anagram(d,c.size()-j-1);
+      
+      
+      
+      if(hantei==1){printf("\n");return 0;}
+    } while( next_permutation(c.begin(), c.end()) );
+    
+    
   }
-  printf("\n");
-
-
-  //printf("%lu",c.size()-j-1);
-  anagram(d,c.size()-j-1);
-
-  // printf("a%d",anagram(d));
-  // printf("b%d",hantei);
-  if (anagram(d,c.size()-j)==1){
-      printf("はい\n");
-      return 0;
-    }
-
-  } while( next_permutation(c.begin(), c.end()) );
-
-
-  }
-
+  
 
 
 }
